9f002b8249768285845dae3169ccc34fe527210a
GabrielVenegas622 <gabriel.venegaso@usm.cl>
2024-11-02 13:47:02 -0300

    falta botón y en home

--- src/pages/home_page.jsx
+++ src/pages/home_page.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
 
+import fwStands from '../assets/feria-software.jpeg'
+
+
 export const HomePage = () => {
   return (
     <>
+      <center><img src={fwStands} width={800} height={450} alt='feria de software' style={{ borderRadius: '15px' }}/></center>
       <p>¡Te damos la bienvenida a la Interfaz energética!</p>
       <p>En esta página encontraras una 💡 ampolleta que podrás encender y apagar con un botón</p>
     </>
