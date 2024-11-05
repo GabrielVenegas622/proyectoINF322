4fadf47c2d9deb6ab95bd80f655f7455aefa871f
jose4366 <jicf2002@gmail.com>
2024-11-05 20:45:42 -0300

    nombre stands

--- src/pages/stand.jsx
+++ src/pages/stand.jsx
@@ -16,24 +16,42 @@ function stand() {
     <div className='stand-page'>
       <div className="grid-wrapper">
         <div className="image-grid">
+          <div className="grid-item">
           <Link to="/page/1">
             <img src= {IATU} alt="Image 1" />
           </Link>
+          <p className="image-name">IATU</p>
+          </div>
+          <div className="grid-item">
           <Link to="/page/2">
-            <img src={DialogApp} alt="Image 2" />
+            <img src= {DialogApp} alt="Image 1" />
           </Link>
+          <p className="image-name"> DialogApp</p>
+          </div>
+          <div className="grid-item">
           <Link to="/page/3">
-            <img src={Canaria} alt="Image 3" />
+            <img src= {Canaria} alt="Image 1" />
           </Link>
+          <p className="image-name">Canaria</p>
+          </div>
+          <div className="grid-item">
           <Link to="/page/4">
-            <img src={GeoTMM} alt="Image 4" />
+            <img src= {GeoTMM} alt="Image 1" />
           </Link>
+          <p className="image-name">GeoTMM</p>
+          </div>
+          <div className="grid-item">
           <Link to="/page/5">
-            <img src={QAGuarIAn} alt="Image 5" />
+            <img src= {QAGuarIAn} alt="Image 1" />
           </Link>
+          <p className="image-name">QAGuarIAn</p>
+          </div>
+          <div className="grid-item">
           <Link to="/page/6">
-            <img src={MineGuard} alt="Image 6" />
+            <img src= {MineGuard} alt="Image 1" />
           </Link>
+          <p className="image-name">MineGuard</p>
+          </div>
         </div>
       </div>
     </div>
